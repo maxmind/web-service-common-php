d0cd82b42cb56c26f869a9f748327f4824c098d1
Gregory Oschwald <goschwald@maxmind.com>
2015-05-22 07:17:42 -0700

    Move shared code from minFraud API to new repo

---
+++ src/Exception/HttpException.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MaxMind\Exception;
+
+/**
+ *  This class represents an HTTP transport error.
+ */
+class HttpException extends WebServiceException
+{
+    /**
+     * The URI queried
+     */
+    public $uri;
+
+    /**
+     * @param string $message A message describing the error.
+     * @param int $httpStatus The HTTP status code of the response
+     * @param string $uri The URI used in the request.
+     * @param \Exception $previous The previous exception, if any.
+     */
+    public function __construct(
+        $message,
+        $httpStatus,
+        $uri,
+        \Exception $previous = null
+    ) {
+        $this->uri = $uri;
+        parent::__construct($message, $httpStatus, $previous);
+    }
+}
---
+++ src/WebService/Http/CurlRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MaxMind\WebService\Http;
+
+
+/**
+ * This class is for internal use only. Semantic versioning does not not apply.
+ * @package MaxMind\WebService\Http
+ * @internal
+ */
+class CurlRequest implements Request
+{
+    private $url;
+    private $options;
+
+    /**
+     * @param $url
+     * @param $options
+     */
+    public function __construct($url, $options)
+    {
+        $this->url = $url;
+        $this->options = $options;
+    }
+
+    /**
+     * @param $body
+     * @return array
+     */
+    public function post($body)
+    {
+        $curl = $this->createCurl();
+
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
+
+        $body = curl_exec($curl);
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        $contentType = curl_getinfo($curl, CURLINFO_CONTENT_TYPE);
+        curl_close($curl);
+
+        return array($statusCode, $contentType, $body);
+    }
+
+    /**
+     * @return resource
+     */
+    private function createCurl()
+    {
+        $curl = curl_init($this->url);
+
+        $opts[CURLOPT_CAINFO] = $this->options['caBundle'];
+        $opts[CURLOPT_SSL_VERIFYHOST] = 2;
+        $opts[CURLOPT_FOLLOWLOCATION] = false;
+        $opts[CURLOPT_SSL_VERIFYPEER] = true;
+        $opts[CURLOPT_RETURNTRANSFER] = true;
+
+
+        $opts[CURLOPT_HTTPHEADER] = $this->options['headers'];
+        $opts[CURLOPT_USERAGENT] = $this->options['userAgent'];
+
+        $opts[CURLOPT_CONNECTTIMEOUT] = $this->options['connectTimeout'];
+        $opts[CURLOPT_TIMEOUT] = $this->options['timeout'];
+
+        curl_setopt_array($curl, $opts);
+        return $curl;
+    }
+}
